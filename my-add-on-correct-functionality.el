9207ad2a4fa8614cd15a4b7bbed39e6b854c816e
Ole P <olepor@matnat.uio.no>
2016-03-28 14:37:51 +0200

    added a flag to stop the recursion

--- my-add-on-correct-functionality.el
+++ my-add-on-correct-functionality.el
@@ -1,6 +1,10 @@
 
-(defvar obligs-home-directory
+;; TODO make the assignment dynamic!
+(defvar obligs-home-directory "~/Downloads/inf1010.2016v.oppg06"
   "A pointer to the directory in which all assignments are downloaded")
+
+(defvar dm-recursion-flag t
+  "Set the flag to false in order to break out of the directory recursion")
 
 (defun set-obligs-home-directory (directory)
   (setq obligs-home-directory directory))
@@ -17,35 +21,46 @@
         )
       )))
 
+(defun dm-correct-student (directory)
+  "The function called when correcting a student"
+  (interactive "Dset the devilry home directory: ")
+  (unless default-directory
+    (setq default-directory directory))
+  (devilry-correct-student directory))
+
+
 
 ;; Check if directory has .java files, if not. recurse down a dir
 ;; only check the top buffer, after we've sorted, and excluded .-buffers
 (defun devilry-correct-student (directory)
   "Corrects the student associated with directory"
-  (interactive "DDirectory to correct:")
   (unless obligs-home-directory
     (set-obligs-home-directory directory))
-  (unzip-if-zipfile-exists directory)
-  (message "The directory passed in is: %s" directory)
-  (let ((current-files-and-attributes (directory-files-and-attributes directory t)))
-    ;; reverse the list, so that the last delivery is handled first.
-    ;; takes advantage of the fact that devilry sorts in descending order,
-    ;; so that no internal sorting is required
-    (setq current-files-and-attributes (nreverse current-files-and-attributes))
-    (dolist (files current-files-and-attributes)
-      (let ((dir-path (car files))
-            (is-dir (car (cdr files))))
-        (message "dir-path %s" dir-path)
-        (unless (string= "." (substring dir-path 0 1))
-          (when is-dir
-            (if (directory-contains-filetype ".java" dir-path)
-                (cond
-                 (t (message "we are in the right dir to correct assignments"))
-                 (t (open-all-files-in-directory))
-                 )
+  (when dm-recursion-flag
+    (unzip-if-zipfile-exists directory)
+    (message "The directory passed in is: %s" directory)
+    (let ((current-files-and-attributes (directory-files-and-attributes directory t)))
+      ;; reverse the list, so that the last delivery is handled first.
+      ;; takes advantage of the fact that devilry sorts in descending order,
+      ;; so that no internal sorting is required
+      (setq current-files-and-attributes (nreverse current-files-and-attributes))
+      (dolist (files current-files-and-attributes)
+        (let ((dir-path (car files))
+              (is-dir (car (cdr files))))
+          (message "dir-path %s" dir-path)
+          (unless (string= "." (substring dir-path 0 1))
+            (when is-dir
+              (if (directory-contains-filetype ".java" dir-path)
+                  (progn
+                   (message "we are in the right dir to correct assignments")
+                   (setq dm-recursion-flag nil) ;; break the recursion at all levels
+                   (message "The recursion flag %S" dm-recursion-flag)
+                   (message "flag above")
+                   (open-all-files-in-directory)
+                   )
                 ;; recurse
                 (devilry-correct-student dir-path)
-                )))))))
+                ))))))))
 
 (message "--------------")
 
